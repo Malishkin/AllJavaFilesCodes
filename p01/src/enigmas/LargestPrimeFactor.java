839cec71a943fa5ac473e893d304eaaebc5ce1dc
Malishkin <malishkin2002@gmail.com>
2020-11-03 20:08:33 +0200

    PrimeFactor

---
+++ p01/src/enigmas/LargestPrimeFactor.java
@@ -0,0 +1,13 @@
+package enigmas;
+
+public class PrimeFactors {
+    public static void main(String[] args) {
+        int number = 315;
+
+    }
+
+    private static void primeFactors(int number) {
+
+
+    }
+}
